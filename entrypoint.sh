af715f9136c72d2961b7caa21c702c5f7128be5c
Timo Salm <tsalm@vmware.com>
2023-07-12 12:06:46 +0200

    Update

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,7 @@ mkdir -p $CRAC_FILES_DIR
 if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
   ( echo 128 > /proc/sys/kernel/ns_last_pid ) 2>/dev/null || while [ $(cat /proc/sys/kernel/ns_last_pid) -lt 128 ]; do :; done
   java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar  /home/app/spring-boot-crac-demo.jar&
-  sleep 5
+  sleep 20
   jcmd /home/app/spring-boot-crac-demo.jar JDK.checkpoint
   sleep 20
 else
