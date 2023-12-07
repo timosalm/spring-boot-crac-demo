80b81dc1d8fa8127b214c8c44f1eeb0472d4f9e5
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-12-07 17:03:34 +0100

    Improvements

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,7 +7,10 @@ mkdir -p $CRAC_FILES_DIR
 
 if [ -z "$(ls -A $CRAC_FILES_DIR)" ]; then
   ( echo 128 > /proc/sys/kernel/ns_last_pid ) 2>/dev/null || while [ $(cat /proc/sys/kernel/ns_last_pid) -lt 128 ]; do :; done
-  java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -Dspring.context.checkpoint=onRefresh -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar  /application/app.jar
+  java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCCheckpointTo=$CRAC_FILES_DIR -jar  /application/app.jar&
+  sleep 60
+  jcmd /application/app.jar JDK.checkpoint
+  sleep 60
 else
   java -Dmanagement.endpoint.health.probes.add-additional-paths="true" -Dmanagement.health.probes.enabled="true" -XX:CRaCRestoreFrom=$CRAC_FILES_DIR&
   PID=$!
