731f4fc2a8ea59bd7f4025ca575d3ffbfea62a2a
Timo Salm <52704922+timosalm@users.noreply.github.com>
2023-12-07 17:01:42 +0100

    Improvements

--- src/main/java/com/example/emojiinclusion/emoji/EmojiRepositoryPopulator.java
+++ src/main/java/com/example/emojiinclusion/emoji/EmojiRepositoryPopulator.java
@@ -1,11 +1,17 @@
 package com.example.emojiinclusion.emoji;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+
 @Component
 public class EmojiRepositoryPopulator implements ApplicationListener<ApplicationReadyEvent> {
+
+    @Autowired
+    DataSource dataSource;
 
     private final EmojiRepository emojiRepository;
 
